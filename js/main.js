018cf6030fa732078b89d6019bf27cb8c1a58548
Vladyslav Levchenko <vladyslav.levchenko@litslink.com>
2020-04-25 16:42:00 +0300

    add modal help

--- js/main.js
+++ js/main.js
@@ -17,15 +17,28 @@ function* menuItemsGenerator(items) {
 const menuItems = [].slice.call(document.getElementsByClassName("flow-item"));
 const menuItemsIterable = menuItemsGenerator(menuItems);
 const btnNext = document.getElementById("btn-next");
+if (btnNext) {
+  btnNext.addEventListener("click", () => {
+    const currentItem = menuItemsIterable.next().value;
+    const currentPos = menuItemsIterable.next();
 
-btnNext.addEventListener("click", () => {
-  const currentItem = menuItemsIterable.next().value;
-  const currentPos = menuItemsIterable.next();
+    if (!(currentPos.value < menuItems.length)) {
+      return;
+    }
+    menuItems[currentPos.value].classList.add("flow-item-active");
+    currentItem.classList.remove("flow-item-active");
+    currentItem.classList.add("flow-item-done");
+  });
+}
 
-  if (!(currentPos.value < menuItems.length)) {
-    return;
-  }
-  menuItems[currentPos.value].classList.add("flow-item-active");
-  currentItem.classList.remove("flow-item-active");
-  currentItem.classList.add("flow-item-done");
-});
+//Toggle help modal
+
+const mainContainer = document.getElementById("main-container");
+const modalHelp = document.getElementById("help-modal");
+
+for (const closeBtn of document.getElementsByClassName("toggle-modal")) {
+  closeBtn.addEventListener("click", function () {
+    mainContainer.classList.toggle("blur");
+    modalHelp.classList.toggle("modal-open");
+  });
+}
